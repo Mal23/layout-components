5d29b10e66b788e19d626897c8f4b5586dfbd761
Mallarie <mallariemilano@gmail.com>
2019-05-13 14:50:08 -0700

    added header component to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Dog from './Dog';
+import Header from './Header';
 
 export default function App() {
   return (
     <>
+      <Header />,
       <Dog />
     </>
   );
